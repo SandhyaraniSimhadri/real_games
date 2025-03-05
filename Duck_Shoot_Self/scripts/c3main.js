b31246443b9788d62035094c67429766f6c314c4
Sandhya Rani Simhadri <sandhyasimhadri999@gmail.com>
2025-03-05 19:11:52 +0530

    games

--- Duck_Shoot_Self/scripts/c3main.js
+++ Duck_Shoot_Self/scripts/c3main.js
@@ -2,3 +2,4 @@ import "./../xmldom.js";
 import "./../xpath.js";
 import "./c3runtime.js";
 import "./objRefTable.js";
+import "./project/scriptsInEvents.js";
---
+++ Duck_Shoot_Self/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async Events_game_Event132_Act2(runtime, localVars)
+	{
+		
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
