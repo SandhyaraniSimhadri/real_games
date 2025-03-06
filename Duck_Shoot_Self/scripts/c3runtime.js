9f6894244f89bdcc929f19aca9fe41bc24109d32
Sandhya Rani Simhadri <sandhyasimhadri999@gmail.com>
2025-03-06 10:55:11 +0530

    games

--- Duck_Shoot_Self/scripts/c3runtime.js
+++ Duck_Shoot_Self/scripts/c3runtime.js
@@ -1621,7 +1621,7 @@ self.C3_ExpressionFuncs = [
 		() => "window.handleGoogleLogin = function (response) {\n    const userCredential = response.credential;\n    const decodedJWT = JSON.parse(atob(userCredential.split('.')[1]));\n\n    console.log('✅ User Signed In: ' + decodedJWT.name);\n\n    // Store user info in Construct 3 Global Variables\n    window.c3_runtime.globalVars.userID = decodedJWT.sub;\n    window.c3_runtime.globalVars.userName = decodedJWT.name;\n    window.c3_runtime.globalVars.userEmail = decodedJWT.email;\n\n    // Call a Construct 3 function to show username\n    window.c3_runtime.callFunction('OnGoogleSignInSuccess', decodedJWT.name);\n};",
 		() => "if (window.googleAPILoaded) {\n    google.accounts.id.prompt();  // Show Google Sign-In popup\n} else {\n    console.log('❌ Google Sign-In API is not ready yet!');\n}\n",
 		() => "not_start",
-		() => "console.log(window.location.href);",
+		() => "var urlParams = new URLSearchParams(window.location.search);\nvar tokenValue = urlParams.get('token');\nconsole.log('Extracted Token:', tokenValue);\nruntime.globalVars.token = tokenValue;",
 		() => "Game with XML",
 		() => "game",
 		() => "xml/your_game.xml",
