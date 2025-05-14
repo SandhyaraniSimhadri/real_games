deedd6076a84e4bdc8a813cb0a8258009b245a5a
Sandhya Rani Simhadri <sandhyasimhadri999@gmail.com>
2025-05-14 12:51:33 +0530

    changes

--- Boat_Rush/c2runtime.js
+++ Boat_Rush/c2runtime.js
@@ -33121,6 +33121,392 @@ cr.plugins_.Spritefont2 = function(runtime)
 
 }());
 
+// AJAX
+// ECMAScript 5 strict mode
+
+;
+;
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.AJAX = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var isNWjs = false;
+	var path = null;
+	var fs = null;
+	var nw_appfolder = "";
+	
+	var pluginProto = cr.plugins_.AJAX.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+		
+		this.lastData = "";
+		this.curTag = "";
+		this.progress = 0;
+		this.timeout = -1;
+		
+		isNWjs = this.runtime.isNWjs;
+		
+		if (isNWjs)
+		{
+			path = require("path");
+			fs = require("fs");
+			var process = window["process"] || nw["process"];
+			nw_appfolder = path["dirname"](process["execPath"]) + "\\";
+		}
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+	
+	instanceProto.onCreate = function()
+	{
+	};
+	
+	instanceProto.saveToJSON = function ()
+	{
+		return { "lastData": this.lastData };
+	};
+	
+	instanceProto.loadFromJSON = function (o)
+	{
+		this.lastData = o["lastData"];
+		this.curTag = "";
+		this.progress = 0;
+	};
+	
+	var next_request_headers = {};
+	var next_override_mime = "";
+	
+	instanceProto.doRequest = function (tag_, url_, method_, data_)
+	{
+		// Create a context object with the tag name and a reference back to this
+		var self = this;
+		var request = null;
+		
+		var doErrorFunc = function ()
+		{
+			self.curTag = tag_;
+			self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnAnyError, self);
+			self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnError, self);
+		};
+		
+		var errorFunc = function ()
+		{
+			// In node-webkit, try looking up the file on disk instead since it wasn't found in the project.
+			if (isNWjs)
+			{
+				var filepath = nw_appfolder + url_;
+				
+				if (fs["existsSync"](filepath))
+				{
+					fs["readFile"](filepath, {"encoding": "utf8"}, function (err, data) {
+						if (err)
+						{
+							doErrorFunc();
+							return;
+						}
+						
+						self.curTag = tag_;
+						self.lastData = data.replace(/\r\n/g, "\n")
+						self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnAnyComplete, self);
+						self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnComplete, self);
+					});
+				}
+				else
+					doErrorFunc();
+			}
+			else
+				doErrorFunc();
+		};
+			
+		var progressFunc = function (e)
+		{
+			if (!e["lengthComputable"])
+				return;
+				
+			self.progress = e.loaded / e.total;
+			self.curTag = tag_;
+			self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnProgress, self);
+		};
+			
+		try
+		{
+			// Windows Phone 8 can't AJAX local files using the standards-based API, but
+			// can if we use the old-school ActiveXObject. So use ActiveX on WP8 only.
+			if (this.runtime.isWindowsPhone8)
+				request = new ActiveXObject("Microsoft.XMLHTTP");
+			else
+				request = new XMLHttpRequest();
+			
+			request.onreadystatechange = function()
+			{
+				if (request.readyState === 4)
+				{
+					self.curTag = tag_;
+					
+					if (request.responseText)
+						self.lastData = request.responseText.replace(/\r\n/g, "\n");		// fix windows style line endings
+					else
+						self.lastData = "";
+					
+					if (request.status >= 400)
+					{
+						self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnAnyError, self);
+						self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnError, self);
+					}
+					else
+					{
+						// In node-webkit, don't trigger 'on success' with empty string if file not found.
+						// In a browser also don't trigger 'on success' if the returned string is empty and the status is 0,
+						// which is what happens when onerror is about to fire.
+						if ((!isNWjs || self.lastData.length) && !(!isNWjs && request.status === 0 && !self.lastData.length))
+						{
+							self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnAnyComplete, self);
+							self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnComplete, self);
+						}
+					}
+				}
+			};
+			
+			if (!this.runtime.isWindowsPhone8)
+			{
+				request.onerror = errorFunc;
+				request.ontimeout = errorFunc;
+				request.onabort = errorFunc;
+				request["onprogress"] = progressFunc;
+			}
+			
+			request.open(method_, url_);
+			
+			if (!this.runtime.isWindowsPhone8)
+			{
+				// IE requires timeout be set after open()
+				if (this.timeout >= 0 && typeof request["timeout"] !== "undefined")
+					request["timeout"] = this.timeout;
+			}
+			
+			request.responseType = "text";
+			
+			if (data_)
+			{
+				if (request["setRequestHeader"] && !next_request_headers.hasOwnProperty("Content-Type"))
+				{
+					request["setRequestHeader"]("Content-Type", "application/x-www-form-urlencoded");
+				}
+			}
+			
+			// Apply custom headers
+			if (request["setRequestHeader"])
+			{
+				var p;
+				for (p in next_request_headers)
+				{
+					if (next_request_headers.hasOwnProperty(p))
+					{
+						try {
+							request["setRequestHeader"](p, next_request_headers[p]);
+						}
+						catch (e) {}
+					}
+				}
+				
+				// Reset for next request
+				next_request_headers = {};
+			}
+			
+			// Apply MIME type override if one set
+			if (next_override_mime && request["overrideMimeType"])
+			{
+				try {
+					request["overrideMimeType"](next_override_mime);
+				}
+				catch (e) {}
+				
+				// Reset for next request
+				next_override_mime = "";
+			}
+
+			if (data_)
+				request.send(data_);
+			else
+				request.send();
+			
+		}
+		catch (e)
+		{
+			errorFunc();
+		}
+	};
+	
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+
+	Cnds.prototype.OnComplete = function (tag)
+	{
+		return cr.equals_nocase(tag, this.curTag);
+	};
+	
+	Cnds.prototype.OnAnyComplete = function (tag)
+	{
+		return true;
+	};
+	
+	Cnds.prototype.OnError = function (tag)
+	{
+		return cr.equals_nocase(tag, this.curTag);
+	};
+	
+	Cnds.prototype.OnAnyError = function (tag)
+	{
+		return true;
+	};
+	
+	Cnds.prototype.OnProgress = function (tag)
+	{
+		return cr.equals_nocase(tag, this.curTag);
+	};
+	
+	pluginProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+
+	Acts.prototype.Request = function (tag_, url_)
+	{
+		var self = this;
+		
+		// In WKWebView mode, if the url is relative (e.g. foo.txt) it should be read as a local file
+		// as RequestFile does. Otherwise it still issues an AJAX request that fails.
+		if (this.runtime.isWKWebView && !this.runtime.isAbsoluteUrl(url_))
+		{
+			this.runtime.fetchLocalFileViaCordovaAsText(url_,
+			function (str)
+			{
+				self.curTag = tag_;
+				self.lastData = str.replace(/\r\n/g, "\n");		// fix windows style line endings
+				
+				self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnAnyComplete, self);
+				self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnComplete, self);
+			},
+			function (err)
+			{
+				self.curTag = tag_;
+				self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnAnyError, self);
+				self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnError, self);
+			});
+		}
+		// In preview mode, also convert relative URLs to local file requests.
+		else if (this.runtime.isPreview && !this.runtime.isAbsoluteUrl(url_))
+		{
+			this.doRequest(tag_, this.runtime.getLocalFileUrl(url_), "GET");
+		}
+		else
+		{
+			this.doRequest(tag_, url_, "GET");
+		}
+	};
+	
+	Acts.prototype.RequestFile = function (tag_, file_)
+	{
+		var self = this;
+		
+		// Use cordova to load local files in WKWebView mode, since AJAX requests will fail.
+		if (this.runtime.isWKWebView)
+		{
+			this.runtime.fetchLocalFileViaCordovaAsText(file_,
+			function (str)
+			{
+				self.curTag = tag_;
+				self.lastData = str.replace(/\r\n/g, "\n");		// fix windows style line endings
+				
+				self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnAnyComplete, self);
+				self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnComplete, self);
+			},
+			function (err)
+			{
+				self.curTag = tag_;
+				self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnAnyError, self);
+				self.runtime.trigger(cr.plugins_.AJAX.prototype.cnds.OnError, self);
+			});
+		}
+		else
+		{
+			this.doRequest(tag_, this.runtime.getLocalFileUrl(file_), "GET");
+		}
+	};
+	
+	Acts.prototype.Post = function (tag_, url_, data_, method_)
+	{
+		this.doRequest(tag_, url_, method_, data_);
+	};
+	
+	Acts.prototype.SetTimeout = function (t)
+	{
+		this.timeout = t * 1000;
+	};
+	
+	Acts.prototype.SetHeader = function (n, v)
+	{
+		next_request_headers[n] = v;
+	};
+	
+	Acts.prototype.OverrideMIMEType = function (m)
+	{
+		next_override_mime = m;
+	};
+	
+	pluginProto.acts = new Acts();
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+
+	Exps.prototype.LastData = function (ret)
+	{
+		ret.set_string(this.lastData);
+	};
+	
+	Exps.prototype.Progress = function (ret)
+	{
+		ret.set_float(this.progress);
+	};
+	
+	Exps.prototype.Tag = function (ret)
+	{
+		ret.set_string(this.curTag);
+	};
+	
+	pluginProto.exps = new Exps();
+
+}());
+
 // Sine
 // ECMAScript 5 strict mode
 
@@ -36195,6 +36581,7 @@ cr.getObjectRefTable = function () {
 		cr.behaviors.scrollto,
 		cr.behaviors.custom,
 		cr.plugins_.Spritefont2,
+		cr.plugins_.AJAX,
 		cr.system_object.prototype.cnds.EveryTick,
 		cr.plugins_.Sprite.prototype.acts.MoveToLayer,
 		cr.system_object.prototype.cnds.OnLayoutStart,
@@ -36207,8 +36594,13 @@ cr.getObjectRefTable = function () {
 		cr.plugins_.TiledBg.prototype.cnds.CompareY,
 		cr.system_object.prototype.cnds.TriggerOnce,
 		cr.plugins_.WebStorage.prototype.acts.StoreLocal,
-		cr.behaviors.Pin.prototype.acts.Pin,
+		cr.plugins_.AJAX.prototype.acts.SetHeader,
 		cr.system_object.prototype.acts.SetVar,
+		cr.plugins_.Browser.prototype.exps.ExecJS,
+		cr.plugins_.Browser.prototype.exps.Platform,
+		cr.plugins_.AJAX.prototype.acts.Post,
+		cr.plugins_.Browser.prototype.acts.GoToURL,
+		cr.behaviors.Pin.prototype.acts.Pin,
 		cr.plugins_.Sprite.prototype.cnds.OnCreated,
 		cr.plugins_.Touch.prototype.cnds.OnTapGestureObject,
 		cr.system_object.prototype.acts.Wait,
@@ -36256,8 +36648,6 @@ cr.getObjectRefTable = function () {
 		cr.plugins_.Browser.prototype.acts.RequestFullScreen,
 		cr.plugins_.Browser.prototype.acts.CancelFullScreen,
 		cr.plugins_.Audio.prototype.acts.SetSilent,
-		cr.plugins_.Browser.prototype.exps.QueryParam,
-		cr.plugins_.Browser.prototype.acts.ConsoleLog,
 		cr.plugins_.WebStorage.prototype.cnds.LocalStorageExists,
 		cr.plugins_.WebStorage.prototype.exps.LocalValue
 	];
@@ -36361,6 +36751,7 @@ cr.getObjectRefTable = function () {
 		{BulletsHud: 0},
 		{SoundHud: 0},
 		{DistanceHud: 0},
+		{AJAX: 0},
 		{Family1: 0},
 		{Family2: 0},
 		{Family3: 0},
@@ -36391,10 +36782,10 @@ cr.getObjectRefTable = function () {
 		{GameOver: 0},
 		{GameStart: 0},
 		{token: 0},
-		{token2: 0},
 		{jsonResponse: 0},
 		{redirect_url: 0},
 		{campaign_id: 0},
+		{jsonBody: 0},
 		{gameKey: 0}
 	];
 
